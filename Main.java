87f22895028475732a782794f9ffbd0420f5e8b7
muntasird <muntasird@nycstudents.net>
2024-10-11 12:29:11 -0400

    work

--- Main.java
+++ Main.java
@@ -6,6 +6,36 @@ public class Main
         Scoreboard game = new Scoreboard("Red", "Blue");
 
 
+        game.recordPlay(1);
+        game.recordPlay(0);
+        game.recordPlay(3);
+        game.recordPlay(1);
+        game.recordPlay(0);
+        game.recordPlay(0);
+        game.recordPlay(4);
+        game.recordPlay(0);
+
+
+        info = game.getScore();
+        System.out.println(info);
+
+        String infoTwo;
+        Scoreboard match = new Scoreboard("Lions", "Tigers");
+
+        //match.recordPlay(1);
+        //match.recordPlay(0);
+        //match.recordPlay(3);
+        //match.recordPlay(1);
+        //match.recordPlay(0);
+        //match.recordPlay(0);
+        //match.recordPlay(4);
+        //match.recordPlay(0);
+
+        infoTwo = match.getScore();
+        System.out.println(infoTwo);
+
+
+
     }
 
 }
--- Scoreboard.java
+++ Scoreboard.java
@@ -2,10 +2,41 @@ public class Scoreboard
 {
     private String teamOne;
     private String teamTwo;
+    private int oneScore;
+    private int twoScore;
+    private String activeTeam;
 
-    /**
-     * constructor
-     */
-    public Scoreboard(String one, String two);
+    public Scoreboard(String one, String two)
+    {
+        teamOne = one;
+        teamTwo = two;
+        activeTeam = teamOne;
+    }
+
+    public String getScore()
+    {
+        return oneScore + "-" + twoScore + "-" + activeTeam;
+    }
+
+    public void recordPlay(int score)
+    {
+        if (teamOne.equals(activeTeam))
+        {
+            oneScore += score;
+        }
+
+        if (teamTwo.equals(activeTeam))
+        {
+            twoScore += score;
+        }
+
+        if (score == 0)
+        {
+            if (teamOne.equals(activeTeam)) activeTeam = teamTwo;
+                    else if (teamTwo.equals(activeTeam)) activeTeam = teamOne;
+        }
+    }
+
+
 }
 
